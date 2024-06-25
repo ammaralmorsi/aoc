bcfdb3649dc0f6d4ba668c694a48a8e9e2bd8eb4
ammaralmorsi <ammaralmorsi@gmail.com>
2024-06-25 22:25:23 +0300

    2021 01-2 cpp

--- 2021/cpp/01/p1.cpp
+++ 2021/cpp/01/p1.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <unordered_set>
+#include <vector>
 
 
 int main() {
+    long long current;
+    long long target = 2020;
+    std::vector<long long> v;
     std::unordered_set<long long> um;
-    long long current, target = 2020;
-    while (std::cin >> current) {
-        if (um.find(target - current) != um.end()) {
-            std::cout << (long long)(current * (target - current)) << std::endl;
-            break;
+
+    while (std::cin >> current)
+        v.push_back(current);
+
+    for (int i = 0; i < v.size(); ++i) {
+        um.clear();
+        for (int j = i + 1; j < v.size(); ++j) {
+            if (um.find(target - v[i] - v[j]) != um.end()) {
+                std::cout << v[j] * (target - v[i] - v[j]) * v[i] << std::endl;
+                goto finish;
+            }
+            um.insert(v[j]);
         }
-        um.insert(current);
     }
+finish:
     return 0;
 }
